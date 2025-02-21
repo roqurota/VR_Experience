13f78aead6f6e850475c3614b129064d8d811ffd
Роман Усольцев <romanusolcev@MacBook-Pro-Roman.local>
2025-02-21 08:54:57 +0100

    rotation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -9,26 +9,28 @@ export default function Experience(){
 
     useFrame((state, delta) => {
         torus.current.rotation.y += delta;
+        cube.current.rotation.x += delta;
+        cube.current.rotation.y += delta;
     })
 
     return <>
-        {/* plane */}
+        
         <mesh position-y={-1} scale={10} rotation-x={Math.PI * 0.5}>
             <planeGeometry scale={10}/>
             <meshBasicMaterial color={'green'}/>
         </mesh>
-        {/* torus */}
+        
         <mesh ref={torus} position-x={-3}>
             <torusKnotGeometry  />
             <meshNormalMaterial />
         </mesh>
-        {/* sphere */}
+        
         <mesh >
             <sphereGeometry />
             <meshBasicMaterial color={'orange'}/>
         </mesh>
-        {/* box */}
-        <mesh position-x={3} >
+        
+        <mesh ref={cube} position-x={3} >
             <boxGeometry />
             <meshBasicMaterial color={'blue'}/>
         </mesh>
