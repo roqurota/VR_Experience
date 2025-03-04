c0076d8485a03d12840b147a9320108110d8c68b
Роман Усольцев <runandback@gmail.com>
2025-03-04 23:06:35 +0100

    tooltip

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -2,10 +2,22 @@ import { Container, Root, Text } from "@react-three/uikit"
 import { Float, RoundedBox } from "@react-three/drei"
 import { Button } from "@react-three/uikit-default"
 import { ChevronRight, ChevronsLeft } from "@react-three/uikit-lucide"
+import { useState, useRef } from "react"
+import { useFrame } from "@react-three/fiber"
+import * as THREE from 'three'
 
 
 export default function Menu() {
-    return <group position={[-.6, 1, -.3]} rotation={[0, 0, 0]}>
+
+    const [color, setColor] = useState('green');
+    const menu = useRef(null);
+
+    useFrame((state) => {
+        menu.current.lookAt(state.camera.getWorldPosition(new THREE.Vector3()))
+    })
+
+
+    return <group ref={menu} position={[-.6, 1, -.3]} rotation={[0, 0, 0]}>
         {/* <RoundedBox
             args={[1, 1, .1]} // Width, height, depth. Default is [1, 1, 1]
             radius={0.05} // Radius of the rounded corners. Default is 0.05
@@ -15,14 +27,14 @@ export default function Menu() {
             >
             <meshPhongMaterial color="#f3f3f3" wireframe />
         </RoundedBox> */}
-        <Root sizeX={1} sizeY={.5} attachCamera={true}>
-            <Button variant="outline" size="icon">
+        <Root sizeX={2} sizeY={.5} attachCamera={true}>
+            <Button variant="outline" size="icon" onClick={() => {setColor('blue')}}>
                 <ChevronsLeft width={16} height={16}/>
             </Button>
-            <Container backgroundColor="red" >
-                <Text fontWeight="bold">Hello World!</Text>
+            <Container backgroundColor={color} >
+                <Text color="white" textAlign="center" fontWeight="bold">Click to change color</Text>
             </Container>
-            <Button variant="outline" size="icon">
+            <Button variant="outline" size="icon" onClick={() => {setColor('red')}}>
                 <ChevronRight width={16} height={16}/>
             </Button>
         </Root>
